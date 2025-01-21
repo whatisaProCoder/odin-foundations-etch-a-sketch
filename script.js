dfaef49b13baadbcbba4d6f2ed23433e7dea55fb
whatisaProCoder <debnathpritam2005@gmail.com>
2025-01-21 06:42:03 +0000

    update

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ for (let i = 0; i < 256; i++) {
     const item = document.createElement("div");
     item.classList.add("item");
     container.appendChild(item);
+
+    item.addEventListener("mouseenter", (event) => {
+        event.target.classList.add("item-hover");
+    })
+
+    item.addEventListener("mouseleave", (event) => {
+        setTimeout(() => {
+            event.target.classList.remove("item-hover");
+        }, 1000);
+    })
 }
